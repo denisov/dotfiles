01fc52920ecc8380b9c1b7ec0ff33e13d7f4375a
Andrey Denisov <elgato.andrey@gmail.com>
2016-02-23 21:17:51 +0600

    initial commit

---
+++ makesymlinks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files=".zshrc"
+
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    echo "Moving existing $file from ~ to $olddir"
+    mv ~/$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
