1420bf447b675066ad7480df0271514d0b370942
Andrey Denisov <elgato.andrey@gmail.com>
2016-10-17 17:54:32 +0700

    lsync conf

--- lsyncd/lsyncd/lsyncd.conf.lua
+++ lsyncd/lsyncd/lsyncd.conf.lua
@@ -1,10 +1,42 @@
 settings {
   nodaemon = true
 }
+-- sync {
+--   default.rsyncssh,
+--   source    = "/home/andrey/rating.n1.ru",
+--   host      = "root@ngs.ru.andr.d",
+--   targetdir = "/data/projects/rating.n1.ru",
+--   delay     = 0,
+--   exclude   = {".git", ".idea"},
+--   rsync     = {
+--     copy_links = true,
+--     perms = true
+--   }
+-- }
+
 sync {
   default.rsyncssh,
-  source    = "/home/andrey/NNN",
+  source    = "/home/andrey/n1.ru",
   host      = "root@ngs.ru.andr.d",
-  targetdir = "/data/projects/NNN",
+  targetdir = "/data/projects/n1.ru",
   delay     = 0,
+  exclude   = {".git", ".idea"},
+  rsync     = {
+    copy_links = true,
+    perms = true
+  }
 }
+
+
+-- sync {
+--   default.rsyncssh,
+--   source    = "/home/andrey/love.ngs.ru",
+--   host      = "root@ngs.ru.andrlove.d",
+--   targetdir = "/data/projects/love.ngs.ru",
+--   delay     = 0,
+--   exclude   = {".git", ".idea"},
+--   rsync     = {
+--     copy_links = true,
+--     perms = true
+--   }
+-- }
